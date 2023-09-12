3334d9a061ed5344c1149c91629feecc7a95bcb9
Dondoo23 <junsu5713@naver.com>
2023-09-12 16:18:54 +0900

    Create 3-4.py
    숫자 카드 게임(이중 for문)

---
+++ 3-4.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split()) # n, m을 공백으로 분리하여 입력
+
+max_value = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+    for item in data:
+        min_value = min(item, min_value) # 이 줄에서 가장 작은 수 찾기
+    max_value = max(max_value, min_value) # 가장 작은 수들 중에서 가장 큰 수 찾기
+
+print(max_value)
