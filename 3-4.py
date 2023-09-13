cbe6e7eb9ded26bcd647374976d67e0807c11605
Dondoo23 <junsu5713@naver.com>
2023-09-13 09:11:07 +0900

    Update 3-4.py

--- 3-4.py
+++ 3-4.py
@@ -6,6 +6,6 @@ for i in range(n):
     min_value = 10001
     for item in data:
         min_value = min(item, min_value) # 이 줄에서 가장 작은 수 찾기
-    max_value = max(max_value, min_value) # 가장 작은 수들 중에서 가장 큰 수 찾기
+    max_value = max(max_value, min_value) # 가장 작은 수들 중에서 가장 큰 수
 
 print(max_value)
